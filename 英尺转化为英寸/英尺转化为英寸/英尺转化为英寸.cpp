fb1ca9cc895e9a35ebce1ba30cf79298efa0ebd7
20172104256 <1969125872@qq.com>
2018-05-22 15:11:52 +0800

    两个英尺英寸的相加
    两个英尺英寸的相加

--- 英尺转化为英寸/英尺转化为英寸/英尺转化为英寸.cpp
+++ 英尺转化为英寸/英尺转化为英寸/英尺转化为英寸.cpp
@@ -14,13 +14,20 @@ public:
 		inch = (meter*3.2808 - feet) * 12;
 		cout << "Ó¢³ß=" << feet << "Ó¢´ç=" << inch << endl;
 	}
-	void funny(double feet, double inch) {
+	/*void funny(double feet, double inch) {
 		if (inch >= 12)
 			feet = feet + (int)(inch / 12);
 		inch = inch - (int)(inch / 12) * 12;
 		cout << "feet=" << feet << " inch=" << inch << endl;
+	}*/
+	void add(double feet1, double feet2, double inch1, double inch2) {
+		feet = feet1 + feet2;
+		inch = inch1 + inch2;
+		if (inch >= 12)
+			feet = feet + (int)(inch / 12);
+		inch = inch - (int)(inch / 12) * 12;
+		cout << "Ó¢³ß=" << feet << " Ó¢´ç=" << inch << endl;
 	}
-
 	void display() {
 		cout << "feet=" << feet << " inch=" << inch << endl;
 	}
@@ -31,7 +38,8 @@ int main()
 	//oc1.display();
 	//oc1.setvalue(500, 600); 
 	//oc1.display();
-	oc2.funny(12, 13);
+	//oc2.funny(12, 13);
+	oc2.add(12, 15, 13, 13);
 	//oc2.display();
 	return 0;
 }
